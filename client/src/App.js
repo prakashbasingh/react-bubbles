e8c6d03bb1e09e416602c337ca28d5c5f12758bc
unknown <prakashbahadursingh2@gmail.com>
2020-05-22 09:25:33 -0700

    private route created to go to the bubblePage. navigation bar is also created to to have a link togo to the login page and protected bubblePage

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,33 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./styles.scss";
 
 import Login from "./components/Login";
-import "./styles.scss";
+import PrivateRoute from "./components/PrivateRoute";
+
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
-      </div>
+      <div>
+        <nav>
+          <div style = {{ margin: '1rem'}}>
+              <link to='/login'>Login</link>
+            </div>
+            <div style = {{ margin: '1rem'}}>
+              <link to='/protected'>Bubble Page (Protected-Page)</link>
+            </div>
+        </nav>
+        <div className="App">
+          <Route exact path="/" component={Login} />
+          {/* 
+            Build a PrivateRoute component that will 
+            display BubblePage when you're authenticated 
+          */}
+          <PrivateRoute exact path='/protected' component={BubblePage}/>
+
+        </div>
+      </div>  
     </Router>
   );
 }
