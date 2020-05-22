16fadb431cba8ed72b68b9120aebef6e8528c846
unknown <prakashbahadursingh2@gmail.com>
2020-05-22 10:36:49 -0700

    private route is imported to the app component which is directed to bubblePage with correct login request, in the bubble page component with axiosWithAuth get request is stablished to fetch color data, with correct username and password bubblePage is rendering

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,22 @@ import "./styles.scss";
 
 import Login from "./components/Login";
 import PrivateRoute from "./components/PrivateRoute";
+import BubblePage from"./components/BubblePage";
+
 
 
 function App() {
   return (
     <Router>
       <div>
+        <h1>React-Bubble</h1>
         <nav>
-          <div style = {{ margin: '1rem'}}>
-              <link to='/login'>Login</link>
-            </div>
-            <div style = {{ margin: '1rem'}}>
-              <link to='/protected'>Bubble Page (Protected-Page)</link>
-            </div>
+            <link to='/login'>Login</link>
+            <link to='/protected'>Bubble Page (Protected-Page)</link>
         </nav>
         <div className="App">
           <Route exact path="/" component={Login} />
-          {/* 
-            Build a PrivateRoute component that will 
-            display BubblePage when you're authenticated 
-          */}
+          {/* Build a PrivateRoute component that will display BubblePage when you're authenticated  */}
           <PrivateRoute exact path='/protected' component={BubblePage}/>
 
         </div>
