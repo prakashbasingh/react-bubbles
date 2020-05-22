1c1f415c1b95255069db06542071ca3ff3f2351f
unknown <prakashbahadursingh2@gmail.com>
2020-05-22 09:10:05 -0700

    login form created with change handler which allow text input in login form, onsubmit handler is also created with axiosWithAuth post request to retrive token and token is saved to local storage then son submit page will be directed to bubblePage. axiosWithAuth function also created to create new instance of the axios module

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,58 @@
 import React from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const Login = () => {
+
+class Login extends React.Component {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+
+  changeHandler = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/api/login", this.state.credentials)
+      .then(res => {
+        console.log(res.data, 'wWWWWWWWWWWWWhat we have here???!!!!????')
+        localStorage.setItem("token", res.data.payload);
+        this.props.history.push("/protected");
+      })
+      .catch(err => console.log(err));
+  };
+
+
+  render() {
+    return (
+      <form onSubmit={this.login}>
+        <input
+          type="text"
+          name="username"
+          value={this.state.credentials.username}
+          onChange={this.changeHandler}
+        />
+        <input
+          type="password"
+          name="password"
+          value={this.state.credentials.password}
+          onChange={this.changeHandler}
+        />
+      <button>Log in</button>
+    </form>
+    );
+  }
 };
 
 export default Login;
